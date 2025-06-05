81249fa0c2cdaea70d51cbc8e801a10865d77d05
ja-das-ist-pb <paulpb0725@gmail.com>
2025-06-05 20:53:48 +0800

    basic struture 1 2025/6/5

--- aftermath/engine.py
+++ aftermath/engine.py
@@ -1,7 +1,82 @@
-#constant pi
+#constant -- pi
 pi=3.1415926535897932
 
-#constant e
+#constant -- e
 e=2.7182818284590452
 
-#basic calculation 
+#basic calculation -- power
+def pow(a, n):
+    ...
+
+#discrete mathematics -- factorial
+def fac(n):
+    ...
+
+#discrete mathematics -- permutation
+def p(n, r):
+    ...
+
+#discrete mathematics -- combination
+def c(n, r):
+    ...
+
+#arithmetic series ** ase("a1", "c"ommon "d"ifference, "l"ength)
+def ase(a1, cd, l):
+    ...
+
+#geometric series ** gse("a1", "c"ommon "r"atio, "l"ength)
+def gse(a1, cr, l):
+    ...
+
+#radian to degree transformation
+
+
+
+
+
+#trigonometric -- sin 
+def sin(x):
+    ...
+
+#trigonometric -- cos
+def cos(x):
+    ...
+
+#trigonometric -- tan
+def tan(x):
+    ...
+
+#trigonometric -- csc
+def csc(x):
+    ...
+
+#trigonometric -- sec
+def sec(x):
+    ...
+
+#trigonometric -- cot
+def cot(x):
+    ...
+
+#trigonometric -- arcsin
+def arcsin(x):
+    ...
+
+#trigonometric -- arccos
+def arccos(x):
+    ...
+
+#trigonometric -- arctan
+def arctan(x):
+    ...
+
+#trigonometric -- arccsc
+def arccsc(x):
+    ...
+
+#trigonometric -- arcsec
+def arcsec(x):
+    ...
+#trigonometric -- arccot
+def arccot(x):
+    ...
